2cf216521889153429c746b355f7e4d0fdc0c2df
changhee <ckdgml228@gmail.com>
2026-02-08 18:44:39 +0900

    vite-plugin-pwa 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,31 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate", // 새 버전 자동 갱신 흐름
+      includeAssets: ["favicon.svg", "favicon.ico", "robots.txt", "apple-touch-icon.png"],
+      manifest: {
+        name: "test-chang",
+        short_name: "test",
+        description: "pwa test chang",
+        theme_color: "#ffffff",
+        background_color: "#ffffff",
+        display: "standalone",
+        scope: "/",
+        start_url: "/",
+        // icons: [
+        //   { src: "/pwa-192x192.png", sizes: "192x192", type: "image/png" },
+        //   { src: "/pwa-512x512.png", sizes: "512x512", type: "image/png" },
+        //   { src: "/pwa-512x512.png", sizes: "512x512", type: "image/png", purpose: "any maskable" }
+        // ]
+      },
+      workbox: {
+        navigateFallback: "/index.html", // SPA 라우팅
+      },
+    }),
+  ],
+});
